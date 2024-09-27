c90ca2d1934b0ba381e970f551596f2ba25c114a
Matthewd340 <146954999+Matthewd340@users.noreply.github.com>
2024-09-27 12:33:38 -0400

    Completed 2.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     float horizontalInput;
     public float speed = 5;
+    public float xRange = 10;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +19,23 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
-        
+        //move player right using acquired horizontal input
         //boolean expression, only runs when true, boolean = true/false
-        if(transform.position.x < -10)
+        if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-
-        if(transform.position.x > 10)
+        //top if statement keeps player above negative xRange, bottom keeps below xRange
+        if(transform.position.x > xRange)
         {
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+            //detects a specific key press
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }               //parameter         parameter           parameter
+                        //object to clone   where to place      what direction
     }
 }
